27cbd3812058089a5f2c2d77ed834ef866a78927
sandeep28988 <sandeepreddy2898@gmail.com>
2026-09-14 20:00:28 +0530

    feat: update front hero image to high-res centered chrome graphic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -123,7 +123,7 @@ export const Home = () => {
                     <a href="#/shop" class="cosmora-hero-banner" aria-label="Shop Accessify Collection">
                         <img 
                             class="cosmora-hero-banner-img" 
-                            src="assets/images/hero-banner.jpg" 
+                            src="assets/images/hero-banner.png?v=3" 
                             alt="Accessify | Premium Streetwear & Gothic Jewelry" 
                         />
                     </a>
